54cb83b7236b539df46ead9e8672227ab284e726
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-10-14 20:03:24 -0400

    Created initial collegescorecard API call

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,31 @@
-$(document).ready(function(){
-console.log("This works")
+$(document).ready(function () {
+  console.log("This works");
 
+  // DOM VARIABLES
 
+  // JS VARIABLES
 
+  // FUNCTION DEFINITIONS
+  function getCollegeInfo() {
+    var city = "athens";
+    var apiKey = "BZXyW8EkmJtygGmoPPNTT8iIeiTbeshMqgalfuXm";
+    var url =
+      "https://api.data.gov/ed/collegescorecard/v1/schools?&_school.city=" +
+      city +
+      "&api_key=" +
+      apiKey;
 
+    $.ajax({
+      url: url,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+    });
+  }
 
+  // FUNCTION CALLS
 
+  getCollegeInfo();
 
-})
+  // EVENT LISTENERS
+});
