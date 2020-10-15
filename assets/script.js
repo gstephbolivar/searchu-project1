416963e5fc7147c120aede483e928dd594ebe20d
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-10-14 21:42:04 -0400

    [WIP] API Call

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,19 @@
-$(document).ready(function(){
-console.log("This works")
+$(document).ready(function () {
+  console.log("This works");
 
+  function searchQuality(cityname) {
+    var queryURL =
+      "https://api.teleport.org/api/cities/?search=" + cityname;
+    // var citySearch
 
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      console.log(queryURL);
+    });
+  }
 
-
-
-
-
-})
+  searchQuality();
+});
