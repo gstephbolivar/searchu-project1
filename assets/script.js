f1a0fdfd17f8ee848498d87f5e8c07d635151782
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-10-17 15:00:48 -0400

    Created code to populate list of university names/info

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
   console.log("This works");
   // DOM VARIABLES
-var userCity = document.getElementById("city-search");
+  var userCity = document.getElementById("city-search");
+
+  var userSchool = $("#school-search");
   // JS VARIABLES
 
   var schoolName;
@@ -17,7 +19,8 @@ var userCity = document.getElementById("city-search");
   // school name has autocomplete -- allows user to enter keywords to get a list of school with those words
   // may be able to use this to autocomplete search field and then search for specific school
   function getCollegeInfo() {
-    var school = "University of Georgia";
+    var school = userSchool.val();
+    console.log("School Searched: " + school);
 
     // api key
     var apiKey = "BZXyW8EkmJtygGmoPPNTT8iIeiTbeshMqgalfuXm";
@@ -25,7 +28,7 @@ var userCity = document.getElementById("city-search");
     // use latest. to get the most recent information
 
     var url =
-      "https://api.data.gov/ed/collegescorecard/v1/schools?_fields=school.name,latest.cost.avg_net_price.overall,latest.admissions.admission_rate.overall,latest.completion.consumer_rate,school.school_url&school.name=" +
+      "https://api.data.gov/ed/collegescorecard/v1/schools?_fields=school.name,school.city,latest.cost.avg_net_price.overall,latest.admissions.admission_rate.overall,latest.completion.consumer_rate,school.school_url&school.name=" +
       school +
       "&api_key=" +
       apiKey;
@@ -34,17 +37,46 @@ var userCity = document.getElementById("city-search");
       url: url,
       method: "GET",
     }).then(function (response) {
-      console.log("2");
-      //console.log(response);
+      //console.log("2");
+      console.log(response);
 
       // loops through results to get the exact school being searched for
       for (var i = 0; i < response.results.length; i++) {
-        if (response.results[i]["school.name"] === school) {
-          console.log(school);
-        }
+        console.log(response.results[i]["school.name"]);
+        schoolName = response.results[i]["school.name"];
+        schoolCity = response.results[i]["school.city"];
+        annualCost = response.results[i]["latest.cost.avg_net_price.overall"];
+        schoolURL = response.results[i]["school.school_url"];
+
+        var newRow = $("<div>")
+          .addClass("row")
+          .attr("style", "background-color: white");
+
+        var newSchool = $("<div>").addClass("col-md-6 m-4");
+
+        newRow.append(newSchool);
+
+        newSchool.append('<h3 id="school">' + schoolName + "</h3>");
+
+        newSchool.append('<h5 id="city">' + "City: " + schoolCity + "</h5>");
+
+        newSchool.append(
+          '<h5 id="avg-cost">' + "Annual Tuition: " + annualCost + "</h5>"
+        );
+
+        newSchool.append(
+          '<a href="' +
+            urlFormat(schoolURL) +
+            '" target="_blank">' +
+            urlFormat(schoolURL) +
+            "</a>"
+        );
+        $("#school-list").append(newRow);
       }
     });
   }
+
+  function populateCollegeInfo() {}
   function getCollegesByCity() {
     // variable to search API by city
     var city = userCity.value;
@@ -62,60 +94,60 @@ var userCity = document.getElementById("city-search");
       url: url,
       method: "GET",
     }).then(function (response) {
-      console.log("1");
-      console.log(response);
+      //console.log("1");
+      //console.log(response);
       // obtains a list of all school names
-        for (var i = 0; i < response.results.length; i++) {
-          schoolName = response.results[i]["school.name"];
-          // schoolCity = response.results[i]["school.city"];
-          annualCost = response.results[i]["latest.cost.avg_net_price.overall"];
-          schoolURL = response.results[i]["school.school_url"];
-          var newRow = $("<div>").addClass("row").attr("style", "background-color: white");
+      for (var i = 0; i < response.results.length; i++) {
+        schoolName = response.results[i]["school.name"];
+        // schoolCity = response.results[i]["school.city"];
+        annualCost = response.results[i]["latest.cost.avg_net_price.overall"];
+        schoolURL = response.results[i]["school.school_url"];
+        var newRow = $("<div>")
+          .addClass("row")
+          .attr("style", "background-color: white");
 
-      var newSchool = $("<div>").addClass("col-md-6 m-4");
+        var newSchool = $("<div>").addClass("col-md-6 m-4");
 
-      newRow.append(newSchool);
+        newRow.append(newSchool);
 
-      newSchool.append(
-        '<h3 id="school">'+ schoolName + "</h3>"
-      );
+        newSchool.append('<h3 id="school">' + schoolName + "</h3>");
 
-      newSchool.append(
-        '<h5 id="avg-cost">' + "Annual Tuition: " + annualCost + "</h5>"
-      );
+        newSchool.append(
+          '<h5 id="avg-cost">' + "Annual Tuition: " + annualCost + "</h5>"
+        );
 
-      newSchool.append(
-        '<a href="'+urlFormat(schoolURL)+'" target="_blank">'+
-          urlFormat(schoolURL) +
-          "</a>"
-      );
+        newSchool.append(
+          '<a href="' +
+            urlFormat(schoolURL) +
+            '" target="_blank">' +
+            urlFormat(schoolURL) +
+            "</a>"
+        );
 
-        function urlFormat(site) {
-          if (site.substring(0,4) === "http"){
-            return site;
-          } else {
-            return "https://"+site;
-          }
-        }
-
-      $("#school-list").append(newRow);
-          // createList(schoolName, annualCost, schoolURL);
-          // admissionsRate =
-          //   response.results[i]["latest.admissions.admission_rate.overall"];
-          // completionRate = response.results[i]["latest.completion.consumer_rate"];
-          // schoolURL = response.results[i]["school.school_url"];
-          // console.log("Name: " + schoolName);
-          // console.log("URL: " + schoolURL);
-          // console.log("City: " + schoolCity);
-          // console.log("Admission Rate: " + admissionsRate);
-          // console.log("Annual Cost: " + annualCost);
-          // console.log("Completion Rate: " + completionRate);
-          // console.log("--------");
-        }
+        $("#school-list").append(newRow);
+        // createList(schoolName, annualCost, schoolURL);
+        // admissionsRate =
+        //   response.results[i]["latest.admissions.admission_rate.overall"];
+        // completionRate = response.results[i]["latest.completion.consumer_rate"];
+        // schoolURL = response.results[i]["school.school_url"];
+        // console.log("Name: " + schoolName);
+        // console.log("URL: " + schoolURL);
+        // console.log("City: " + schoolCity);
+        // console.log("Admission Rate: " + admissionsRate);
+        // console.log("Annual Cost: " + annualCost);
+        // console.log("Completion Rate: " + completionRate);
+        // console.log("--------");
+      }
     });
   }
+  function urlFormat(site) {
+    if (site.substring(0, 4) === "http") {
+      return site;
+    } else {
+      return "https://" + site;
+    }
+  }
 
- 
   // Gets the city or cities if more than one with the same name
   function getQualityOfLife() {
     var city = "Atlanta";
@@ -125,8 +157,8 @@ var userCity = document.getElementById("city-search");
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-      console.log(response);
-      console.log(queryURL);
+      //console.log(response);
+      //console.log(queryURL);
       for (
         var i = 0;
         i < response._embedded["city:search-results"].length;
@@ -134,7 +166,7 @@ var userCity = document.getElementById("city-search");
       ) {
         var cityName =
           response._embedded["city:search-results"][i].matching_full_name;
-        console.log("City: " + cityName);
+        //console.log("City: " + cityName);
       }
     });
   }
@@ -143,7 +175,6 @@ var userCity = document.getElementById("city-search");
 
   getCollegesByCity();
   console.log("colleges");
-  getCollegeInfo();
 
   getQualityOfLife();
 
@@ -153,5 +184,12 @@ var userCity = document.getElementById("city-search");
     $("#home-page").addClass("d-none");
     $("#school-list").removeClass("d-none");
     getCollegesByCity();
+  });
+
+  $("#submit-school").on("click", function (event) {
+    event.preventDefault();
+    $("#home-page").addClass("d-none");
+    $("#school-list").removeClass("d-none");
+    getCollegeInfo();
   });
 });
